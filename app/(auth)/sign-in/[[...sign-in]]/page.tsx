861783a776d4c0aa631f0a9cc923c96e0200c9ea
Luis Starlino <luis.guilherme009@gmail.com>
2025-04-22 22:40:07 -0300

    finish login 2

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ import useLoadingPage from '@/hooks/use-loading';
 import React, { useEffect } from 'react'
 import { SignIn } from '@clerk/nextjs'
 
-const page = () => {
+const Page = () => {
 
   useEffect(() => {
     if (loadingController.isOpen) return loadingController.onClose();
@@ -19,4 +19,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page;
